5324efcc1f217ac183e95bcd0c9b8d4c1bae2e3d
Rasmus Rask <rasse.rask@gmail.com>
2024-05-24 14:01:40 +0300

    <feat> Added controls for moving cube by a set speed.

--- frontend/pong_threejs/main.js
+++ frontend/pong_threejs/main.js
@@ -1,16 +1,18 @@
 import * as THREE from 'three';
 
+const speed = 2.5
+
 const scene = new THREE.Scene()
 
 const geometry = new THREE.BoxGeometry(5, 5, 5)
-const material = new THREE.MeshStandardMaterial({color: 0xFF0000})
+const material = new THREE.MeshStandardMaterial({color: 0xFF0000, roughness: 0.5, metalness: 0., emissive: 0x00})
 const cube = new THREE.Mesh(geometry, material)
 
 const light = new THREE.DirectionalLight(0xFFFFFF, 1)
 light.position.set(0, 2, 200)
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 20
+const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 50
 
 scene.add(camera, cube, light)
 
@@ -18,11 +20,24 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+
+//Function for handling keypress
+const handleKeyPress = (e) => {
+	switch(e.key) {
+		case 'w':
+			cube.position.y += speed
+			break
+		case 's':
+			cube.position.y -= speed
+			break
+		}
+}
+
+//Event listener for keypress
+document.addEventListener('keypress', handleKeyPress)
+
 function animate() {
 	requestAnimationFrame(animate)
-	cube.rotation.x += 0.01
-	cube.rotation.y += 0.01
-
 	renderer.render(scene, camera)
 }
 
